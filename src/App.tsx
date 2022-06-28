fc31ec5a85f4bd2d9a0c3d0ab245acf7ef593d99
sallyno <r09078433806@gmail.com>
2022-06-28 21:29:08 +0900

    不要ファイル削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div>
+          <button id="updAttendance">出勤時間登録</button>
+          <button id="updLeaving">退勤時間登録</button>
+        </div>
     </div>
   );
 }
