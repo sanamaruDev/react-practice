93d1828807edd59d339e2110b3b3386d39cb0b7c
sallyno <r09078433806@gmail.com>
2022-07-02 23:37:02 +0900

    メニューデザイン変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
-import { Link } from "react-router-dom";
-import { Box } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
+import { Box, Button } from "@chakra-ui/react";
 
 import './App.css';
 
 export const App = () => {
+  const navigate = useNavigate()
   return (
-    <Box justifyContent="center" margin="8">
-      <Link to="/attendance">出勤時間登録</Link>
-      <Link to="/leaving">退勤時間登録</Link>
-      <Link to="/workingtimelist">一覧表示</Link>
-    </Box>
+    <div>
+      <Box display="flex" justifyContent="center" margin="8">
+        <Button onClick={() => navigate('/attendance')}>出勤時間登録</Button>
+      </Box>
+      <Box display="flex" justifyContent="center" margin="8">
+        <Button onClick={() => navigate('/leaving')}>退勤時間登録</Button>
+      </Box>
+      <Box display="flex" justifyContent="center" margin="8">
+        <Button onClick={() => navigate('/workingtimelist')}>一覧表示</Button>
+      </Box>
+    </div>
   );
 };
