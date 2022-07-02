35674e9d11df553fdca986ac6656093984e3379d
sallyno <r09078433806@gmail.com>
2022-07-02 23:18:44 +0900

    post仮実装

--- src/pages/Attendance.tsx
+++ src/pages/Attendance.tsx
@@ -1,8 +1,41 @@
 import { Box, Text, Select, Button } from "@chakra-ui/react"
+import React from "react";
+import axios from "axios";
 
 const today = new Date();
+const postUrl = "http://xxx";
 
 export const Attendance = () => {
+
+    // userState
+    const [hour, setHour] = React.useState("");
+    const [time, setTime] = React.useState("");
+
+    const handleChangeHour = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        setHour(e.target.value)
+    }
+
+    const handleChangeTime = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        setTime(e.target.value)
+    }
+
+    // selectBox
+    // const aryHour = [08,09,10,11,12,13,14,15];
+    // const aryTime = [00,15,30,45];
+
+    const regist = () => {
+        axios.post(postUrl, {
+            hour: hour,
+            time: time
+        })
+        .then((response) => {
+            console.log(response);
+        })
+        .catch((response) => {
+            console.log(response);
+        })
+    }
+
     return (
         <div>
             <Box display="flex" justifyContent="center" margin="8">
@@ -10,7 +43,7 @@ export const Attendance = () => {
                 <Text fontSize='3xl' fontFamily="Meiryo" marginInline="2">出勤時間を設定</Text>
             </Box>
             <Box display="flex" justifyContent="center" margin="8">
-                <Select placeholder='select hour'>          
+                <Select placeholder='select hour' onChange={(e) => handleChangeHour(e)}>
                     <option value='08'>08</option>
                     <option value='09'>09</option>
                     <option value='10'>10</option>
@@ -21,7 +54,7 @@ export const Attendance = () => {
                     <option value='15'>15</option>
                 </Select>
                 <Text fontSize="3xl">：</Text>
-                <Select placeholder='select time'>          
+                <Select placeholder='select time' onChange={(e) => handleChangeTime(e)}>          
                     <option value='00'>00</option>
                     <option value='15'>15</option>
                     <option value='30'>30</option>
@@ -29,7 +62,7 @@ export const Attendance = () => {
                 </Select>
             </Box>
             <Box display="flex" justifyContent="center" margin="8">
-                <Button>出勤時間登録</Button>
+                <Button onClick={regist}>出勤時間登録</Button>
             </Box>
         </div>
     );
